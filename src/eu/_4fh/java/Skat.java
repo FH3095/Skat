809b6e9169a58a22b8efa24a75659d48eadf651d
FH <fh3095-github@yahoo.de>
2011-05-27 19:03:10 +0200

    First commit

---
+++ src/eu/_4fh/java/Skat.java
@@ -0,0 +1,14 @@
+package eu._4fh.java;
+
+/**
+ *
+ * @author FH
+ */
+public class Skat {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+	}
+}
